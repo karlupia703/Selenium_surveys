988ecf6d1846384f35157928bcaf467253175d99
Rajni <karlupiarajni12@gmail.com>
2025-01-03 18:05:14 +0530

    Refactor code

--- webtestNG/src/test/java/test/Maindriverclass.java
+++ webtestNG/src/test/java/test/Maindriverclass.java
@@ -10,37 +10,29 @@ public class Maindriverclass {
 	private static final By By = findFirstRow;
 	public static WebDriver driver;
     public static void main(String[] args) throws InterruptedException {
+    
+    	ReinstatementLoginTest loginTest = new ReinstatementLoginTest();
+        loginTest.setup(); // Initialize and log in
+      loginTest.loginuser();
+      Thread.sleep(5000);
+      
+      createUserTest createUserTest = new createUserTest();
+      createUserTest.testCreateUser();
+      Thread.sleep(2000);
+      createUserTest.editUser(By);
+      Thread.sleep(2000);
+      createUserTest.viewUser();
+      Thread.sleep(3000);
+      createUserTest.deleteUser();
+      Thread.sleep(3000);
+      createUserTest.testcaseOfFilter();
+      Thread.sleep(3000);
+      createUserTest.searchUser1();
+      Thread.sleep(3000);
+      createUserTest.pagination();
+      Thread.sleep(3000);
+      createUserTest.testAlreadyExistUser();
+      Thread.sleep(3000);	
     	
-    	
-        try {
-            // Run ReinstatementLoginTest
-            ReinstatementLoginTest loginTest = new ReinstatementLoginTest();
-            loginTest.setup(); // Initialize and log in
-            loginTest.loginuser();
-            Thread.sleep(5000);
-            
-            createUserTest createUserTest = new createUserTest();
-            createUserTest.testCreateUser();
-            Thread.sleep(2000);
-            createUserTest.editUser(By);
-            Thread.sleep(2000);
-            createUserTest.viewUser();
-            Thread.sleep(3000);
-            createUserTest.deleteUser();
-            Thread.sleep(3000);
-            createUserTest.testcaseOfFilter();
-            Thread.sleep(3000);
-            createUserTest.searchUser1();
-            Thread.sleep(3000);
-            createUserTest.pagination();
-            Thread.sleep(3000);
-            createUserTest.testAlreadyExistUser();
-            Thread.sleep(3000);
-            
-        } finally {
-            // Close the WebDriver
-          //  DriverManager.quitDriver();
-        	System.out.println("All testcase are run sucessfully.");
-        }
     }
 }
